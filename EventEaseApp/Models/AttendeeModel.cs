f6c91edfc1c3b7e13546cbf0abb91c673e7ef11a
Raitis Skrebels <raitis0303@inbox.lv>
2025-04-18 21:40:54 +0300

    Attendance Tracker added

---
+++ EventEaseApp/Models/AttendeeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace EventEaseApp.Models
+{
+    public class AttendeeModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public bool Attended { get; set; }
+    }
+}
---
+++ EventEaseApp/Services/AttendanceService.cs
@@ -0,0 +1,23 @@
+﻿using EventEaseApp.Models;
+
+namespace EventEaseApp.Services
+{
+    public class AttendanceService
+    {
+        private List<AttendeeModel> attendees = new();
+
+        public void Register(string name)
+        {
+            attendees.Add(new AttendeeModel { Name = name });
+        }
+
+        public List<AttendeeModel> GetAll() => attendees;
+
+        public void MarkAttended(string name)
+        {
+            var person = attendees.FirstOrDefault(a => a.Name == name);
+            if (person != null)
+                person.Attended = true;
+        }
+    }
+}
