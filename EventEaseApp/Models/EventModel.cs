6850b800f10cba8da6ad4e9b3e22b99220f5811a
Raitis Skrebels <raitis0303@inbox.lv>
2025-04-18 21:23:07 +0300

    Add project files.

---
+++ EventEaseApp/Models/EventModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEaseApp.Models
+{
+    public class EventModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Description { get; set; }
+        [Required]
+        public string Location { get; set; }
+    }
+}
---
+++ EventEaseApp/Models/EventRegistrationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEaseApp.Models
+{
+    public class EventRegistrationModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please select an event.")]
+        public int? EventId { get; set; }
+    }
+}
